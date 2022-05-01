df2054a840bb45613a7ea7ce2fa0624a89e97a42
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2022-05-01 16:48:16 +0200

    raise error on sync

--- src/main/webapp/sync.js
+++ src/main/webapp/sync.js
@@ -6,9 +6,15 @@ console.log('[React sync] Sync frontend. current folder = ', process.cwd())
 const rsync = new Rsync()
     .archive()
     .source('build/*')
-    .destination('../resources/META-INF/resources');
+    .destination('../resources/META-INF/resources')
+    .flags('v', true);
 
 // Execute the command
-rsync.execute(function(error, code, cmd) {
-    console.log('[React sync] All done executing ', cmd);
+rsync.execute(function(error, code, stderr) {
+    console.log('[React sync] All done executing ', code);
+    if( code !== 0) {
+        console.error(stderr, error);
+        throw Error(stderr);
+    }
 });
+
